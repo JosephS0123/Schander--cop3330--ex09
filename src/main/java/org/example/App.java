63a9c93d9fb0d9b07ebe6e59821733da8353f2b3
Joseph <JosephSchander@gmail.com>
2021-09-11 16:15:13 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        Scanner uInput = new Scanner(System.in);
+        System.out.println( "What is the width of the room?" );
+        int Width = uInput.nextInt();
+        System.out.println("What is the length of the room?");
+        int Length = uInput.nextInt();
+        int Sqrf = (Length * Width);
+        int Sqrf2 = Sqrf;
+        int N = 0;
+        while (Sqrf > 0){
+            Sqrf = Sqrf - 350;
+            N++;
+        }
+        System.out.println("You will need "+ N + " gallons of paint to cover "+ Sqrf2 + " square feet");
+    }
+}
